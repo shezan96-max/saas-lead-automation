ff8eee04aeda6741c716468185bc3ac7e8a5bc7b
Shezan <shezansheikh9693@gmail.com>
2026-02-19 01:29:52 +0600

    Brevo SMTP setup

--- automation/email.py
+++ automation/email.py
@@ -1,13 +1,1 @@
-import os
-import resend
-from config.config import RESEND_API_KEY
-
-resend.api_key = RESEND_API_KEY
-
-def send_email(to_email,subject,body):
-    resend.Emails.send({
-        "from": "Automation<onboarding@resend.dev>",
-        "to": [to_email],
-        "subject": subject,
-        "html": f"<p>{body}</p>" 
-    })
+import smtplib
