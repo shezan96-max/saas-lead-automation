e8a1fd4aca8b163c57088fc2e1317acb949ed941
Shezan <shezansheikh9693@gmail.com>
2026-02-19 01:38:09 +0600

    Fix email import

--- automation/email.py
+++ automation/email.py
@@ -1,1 +1,22 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from config.config import EMAIL_SENDER,EMAIL_PASSWORD
+
+SMTP_SERVER = "smtp-relay.brevo.com"
+SMTP_PORT = 587
+
+def send_email(to_email,subject,body):
+    message = MIMEMultipart()
+    message["From"] = EMAIL_SENDER
+    message["To"] = to_email
+    message["Subject"] = subject
+    
+    message.attach(MIMEText(body,"plain"))
+
+    server = smtplib.SMTP(SMTP_SERVER,SMTP_PORT)
+    server.starttls()
+    server.login(EMAIL_SENDER,EMAIL_PASSWORD)
+    server.sendmail(EMAIL_SENDER,to_email,message.as_string())
+    server.quit()
+    
