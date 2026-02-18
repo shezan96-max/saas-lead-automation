27a1287d5df3257f64a56d66801e8653f86bb176
Shezan <shezansheikh9693@gmail.com>
2026-02-19 02:19:15 +0600

    Fix Brevo timeout

--- automation/email.py
+++ automation/email.py
@@ -1,24 +1,26 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from config.config import EMAIL_SENDER,EMAIL_PASSWORD
-
-SMTP_SERVER = "smtp-relay.brevo.com"
-SMTP_PORT = 587
+import requests
+from config.config import BREVO_API_KEY
 
 def send_email(to_email,subject,body):
-    message = MIMEMultipart()
-    message["From"] = EMAIL_SENDER
-    message["To"] = to_email
-    message["Subject"] = subject
-    
-    message.attach(MIMEText(body,"plain"))
-    try:
-        server = smtplib.SMTP(SMTP_SERVER,SMTP_PORT,timeout=30)
-        server.starttls()
-        server.login(EMAIL_SENDER,EMAIL_PASSWORD)
-        server.sendmail(EMAIL_SENDER,to_email,message.as_string())
-        server.quit()
-    except Exception as e:
-        print("Email error:",str(e))
-        raise e
+    url = "https://api.brevo.com/v3/smtp/email"
+
+    headers = {
+        "accept": "application/json",
+        "api-key": BREVO_API_KEY,
+        "content-type": "application/json"
+    }
+
+    payload = {
+        "sender": {"email":"agentsupremo21@gmail.com"},
+        "to": [{"email":to_email}],
+        "subject": subject,
+        "textContent": body
+    }
+
+    response = requests.post(url,json=payload,headers=headers,timout=15)
+
+    print(response.status_code)
+    print(response.text)
+
+    if response.status_code not in [200,201]:
+        raise Exception("Email sending failed")
