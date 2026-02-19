57e8fb65c74565429230b0c772c327783ce71eff
Shezan <shezansheikh9693@gmail.com>
2026-02-20 00:43:25 +0600

    migrate ro psycopg3 for python 3.14 compatibility

--- database/db.py
+++ database/db.py
@@ -1,9 +1,9 @@
-import psycopg2
+import psycopg
 from config.config import DATABASE_URL
 
 
 def get_connection():
-    return psycopg2.connect(DATABASE_URL)
+    return psycopg.connect(DATABASE_URL)
 
 
 def init_db():
